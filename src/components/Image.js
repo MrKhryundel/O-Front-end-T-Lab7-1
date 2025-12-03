f72e88d3f38834c5ae71bc2a2c8bd74a2634599a
MrKhryundel <zahvatkindaniil016@gmail.com>
2025-12-04 00:42:25 +0200

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import lvivPhoto from "C:/KPI/3y_kurs/1semestr/O-Front-end-T/Lab7/lab7_1/src/images/4cd6916ff31ce68b7a0043c4df272f90374f56e6_1585229828.png";
+import lvivPhoto from "./images/4cd6916ff31ce68b7a0043c4df272f90374f56e6_1585229828.png";
 
 function Image() {
     const [imgWidth, setImgWidth] = useState(600);
@@ -27,5 +27,6 @@ function Image() {
 }
 
 export default Image;
+
 
 
